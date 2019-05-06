9e02f99d66094b8df720dacd4a752a39d248e8a7
Tayfun <yurdaer@zoho.com>
2019-05-06 22:47:36 +0300

    "LOCAL" word removed from env keys

--- webapp/settings_development.py
+++ webapp/settings_development.py
@@ -6,11 +6,11 @@ DEBUG = True
 DATABASES = {
     'default': {
     'ENGINE': 'django.db.backends.mysql', 
-    'NAME': os.getenv("LOCAL_DB_NAME", 'news_feed_db'),
-    'USER': os.getenv("LOCAL_DB_USER"),
-    'PASSWORD': os.getenv("LOCAL_DB_PASSWORD"),
-    'HOST': os.getenv("LOCAL_DB_HOST", 'database'),
-    'PORT': os.getenv("LOCAL_DB_PORT", 3306),
+    'NAME': os.getenv("DB_NAME", 'news_feed_db'),
+    'USER': os.getenv("DB_USER"),
+    'PASSWORD': os.getenv("DB_PASSWORD"),
+    'HOST': os.getenv("DB_HOST", 'database'),
+    'PORT': os.getenv("DB_PORT", 3306),
   }
 }
 
