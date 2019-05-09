9ca19698e3bc7fe493d6b1ff37a369b068200f05
Tayfun <yurdaer@zoho.com>
2019-05-09 18:23:22 +0300

    debugging environment is set up

--- manage.py
+++ manage.py
@@ -8,6 +8,11 @@ def main():
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'webapp.settings')
     try:
         from django.core.management import execute_from_command_line
+
+        if os.getenv("APP_ENV", 'PROD') is not "PROD":
+            import ptvsd
+            ptvsd.enable_attach(address=('0.0.0.0', 3500))
+
     except ImportError as exc:
         raise ImportError(
             "Couldn't import Django. Are you sure it's installed and "
