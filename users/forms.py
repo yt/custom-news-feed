5657da781697742daec9c20b6ecd051a304447f3
Tayfun <typhoon9721@gmail.com>
2019-06-27 00:01:30 +0300

    feat/user page and authentication is completed

--- users/forms.py
+++ users/forms.py
@@ -8,3 +8,7 @@ class RegisterForm(forms.Form):
     first_name = forms.CharField(widget=forms.TextInput(attrs={'class':'form-control'}))
     last_name = forms.CharField(widget=forms.TextInput(attrs={'class':'form-control'}))
     phone_number = forms.CharField(widget=forms.NumberInput(attrs={'class':'form-control'}), required=False)
+
+class LoginForm(forms.Form):
+    username = forms.CharField(widget=forms.TextInput(attrs={'class':'form-control'}))
+    password = forms.CharField(widget=forms.PasswordInput(attrs={'class':'form-control'}))
